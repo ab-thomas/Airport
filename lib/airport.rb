723603cfa50c4c0b3c6c8a18ee17b8aa08366b2f
Abraham Thomas <dysruptmedia@gmail.com>
2014-01-26 18:14:14 +0000

    airport added

---
+++ lib/airport.rb
@@ -0,0 +1,45 @@
+class Airport
+
+  def initialize(planes = {})    
+    self.capacity = planes.fetch(:capacity, capacity)
+  end
+
+  
+  DEFAULT_CAPACITY = 50
+  
+  
+  def planes
+    @planes ||= [] 
+
+  def capacity
+    @capacity ||= DEFAULT_CAPACITY
+  end
+
+  def capacity=(value)    
+    @capacity = value
+  end
+
+  def has_planes? 
+    @planes  
+  end
+
+  def dock(plane)
+    raise "Airport is full" if full?
+    planes << plane
+  end
+
+  def planes_count 
+    planes.count
+  end
+
+  def release(plane) 
+    planes.delete(plane)
+  end
+
+  def full? 
+    planes.count == capacity 
+  end
+
+
+
+end
