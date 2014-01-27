f8b1b9b996cd6db763e209829b8b0c3e68e96a5d
Abraham Thomas <dysruptmedia@gmail.com>
2014-01-27 09:50:48 +0000

    Weather_sample method

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,16 +1,7 @@
 module Weather
-# set weather as constants 
-    STORMY = 1
-    SUNNY  = 2
 
-# Create method to randomise weather 
-# to be called by airport.rb
-  def weather
-     r = Random.new
-      r.rand(1...2)
-  end
+  weather = ["Sunny", "Stormy"]
 
-  def sunny?
-  end
+  weather.sample # research method
 
 end
